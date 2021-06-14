133c8f0a7ff34e6466b55f2a5139e8dcbb33527a
ottobonbifaria <ottobonifaria@live.com>
2021-06-14 13:41:22 -0300

    adiononado logo zup e revisado aparencia

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
+container = document.querySelector('.container');
+
 /* Fixa o numero maximo de personagens possíveis*/
 const numeroMaximoDePersonagens = 671;
 
-/* Busca Informações de 4 personagens aleatoriamente*/
-for (var i = 0; i < 4; i++) {
-    // gera valor aleatorio //
-    const idAleatorio = Math.floor(Math.random() * numeroMaximoDePersonagens);
+const gerarIdAleatorioDePersonagem = () => {
+    return Math.floor(Math.random() * numeroMaximoDePersonagens);
+}
 
-    /* Conecta a API à nossa pagina */
-    fetch(`https://rickandmortyapi.com/api/character/${idAleatorio}`, {
+const buscarPersonagemAleatorio = () => {
+    const idAleatorioDePersonagem = gerarIdAleatorioDePersonagem();
+    return fetch(`https://rickandmortyapi.com/api/character/${idAleatorioDePersonagem}`, {
         method: 'GET',
         headers: {
             Accept: 'application/jason',
             "contente-Type": 'aplication/jason'
         }
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            var container = document.querySelector('.container');
-            container.innerHTML += `<div class="card">
-                                        <img src="` + data.image + `">
-                                        <table>
-                                            <tr><td><strong>Nome:</strong></td><td>`+ data.name + `</td></tr>
-                                            <tr><td><strong>Status:</strong></td><td>`+ data.status + `</td></tr>
-                                            <tr><td><strong>Espécie:</strong></td><td>`+ data.species + `</td></tr>
-                                        </table>
-                                    </div>`;
-        })
+    }).then((response) => response.json())
+}
+
+const incluirNovoCard = (data) => {
+    container.innerHTML +=  `<div class="card">
+                                <img src="` + data.image + `">
+                                <table>
+                                    <tr><td><strong>Nome:</strong></td><td><b style="color:black" >`+ data.name + `</b></td></tr>
+                                    <tr><td><strong>Status:</strong></td><td><b style="color:black" >`+ data.status + `</b></td></tr>
+                                    <tr><td><strong>Espécie:</strong></td><td><b style="color:black" >`+ data.species + `</b></td></tr>
+                                    <tr><td><strong>Gênero:</strong></td><td><b style="color:black" >`+ data.gender + `</b></td></tr>
+                                </table>
+                            </div>`;
+}
+
+/* Busca Informações de 4 personagens aleatoriamente e inclui os cards de cada*/
+for (var i = 0; i < 4; i++) {
+    buscarPersonagemAleatorio()
+        .then((data) => incluirNovoCard(data))
 }
